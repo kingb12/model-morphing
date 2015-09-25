afee7720a22936aa34326cb1761ebf9d6f4c9d1e
kingb12 <kingb12@cs.washington.edu>
2015-09-25 23:36:07 +0000

    More modularization and documentation. Made several method stubs to be implemented

--- lib/Client.py
+++ lib/Client.py
@@ -34,7 +34,72 @@ def morph_model(args):
     morph = Morph()
     _label_reactions(morph)
 
+def _translate_features(morph):
+    """
+    Translates the features in the source model to matches in the target genome
+
+    NOTE: The translated model will lose reactions in the source model that did not have matching features
+    and is not guaranteed to be useful in FBA simulation.
+
+    Requires:
+        morph.model, morph.modelws must form a valid ObjectIdentity. morph.protcomp/morph.protcompws must form a valid ObjectIdentity
+    Modifies:
+        morph - morph.trans_model
+    """
+def _reconstruct_genome(morph):
+    """
+    Builds a draft reconstruction from the genome in the morph.
+
+    Builds a draft model reconstruction and saves it in the workspace associated with morph.ws_id.
+
+    Requires:
+        morph.genome and morph.genomews must be valid ID's for KBase objects/workspace (respectively), forming a valid
+        ObjectIdentity.
+    Modifies:
+        morph - morph.recon
+    """
+def _get_objects(morph):
+    """
+    Gets the model and probanno objects associated with the morph.
+
+    Populates morph.objects with a dictionary of objects. Three are FBA Models, with keys 'source_model', 'recon_model', 'trans_model'.
+    The last is a ProbAnno RxnProbs object, keyed 'probanno'. Corresponding entries are put in a dictionary morph.info, with the object_info
+    as it is returned from the workspace service"
+
+    Modifies:
+        morph - morph.objects and morph.info
+
+    Requires:
+        model, trans_model, recon, and probanno are all attributes of morph and valid IDs for KBase objects. modelws and probannows are valid
+        workspace ID's corresponding with model and probanno attributes of morph to form valid ObjectIdentity's.
+
+   :param morph: the Morph object with the associated objects.
+    """
+
 def _label_reactions(morph):
+    """
+    Labels the reactions in the translated model and the reconstruction
+
+    Populates the rxn_labels attribute in the Morph object with a Dictionary of four lists of tuples. the lists are named with the keys:
+        - gene-match
+        - gene-no-match
+        - no-gene
+        - recon
+
+    Each list contains tuples as such: (reaction_id, model_index, probability)
+    Where reaction_id is a kbase reaction id with the compartment info appended to the end (e.g. rxn01316_c0),
+    model_index is the index of the reaction in the objects['x_model'][modelreactions] list, and the probability
+    is the reaction probability associated with each reaction from the probanno object. Reactions not in ProbAnno
+    are given an arbitrary probability of -1.0
+
+    Requires: Morph.objects and Morph.info must have valid entries for 'source_model', 'trans_model', 'recon_model',
+    and 'probanno' (The post condition of _get_objects()).
+
+    Modifies:
+        Morph - Morph.rxn_labels
+
+    :param morph: the Morph object for labeling as described above
+    """
     label_time = time.time()
     # translate the model using Kbase translation (This translates the features
     # for the model as well)
@@ -44,4 +109,5 @@ def _label_reactions(morph):
     morph.info['source_model'] = object['info']
     translation_params = {'keep_nogene_rxn': 1, 'protcomp': morph.protcomp, 'protcomp_workspace': morph.protcompws, 'model': morph.model, 'model_workspace': morph.modelws, 'workspace': ws_id}
     morph.trans_model =  fba_client.translate_fbamodel(translation_params)[0]
+
 ws_client, fba_client = _init_clients()
