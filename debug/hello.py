e47fa78bdf4f8307c171eaf540be10d903a27833
kingb12 <kingb12@cs.washington.edu>
2015-10-12 20:50:35 +0000

    removed debug files

--- debug/hello.py
+++
@@ -1,9 +0,0 @@
-from biokbase.workspace.client import Workspace 
-params = dict()
-# params['workspace'] = 'eureka'
-ws = Workspace()
-# b = ws.create_workspace(params)
-a = ws.list_workspace_info(params)
-for i in a:
-	print i
-
--- debug/list_objs_by_ID.py
+++
@@ -1,8 +0,0 @@
-from biokbase.workspace.client import Workspace 
-import sys
-ws_client = Workspace()
-ws_idsa = list()
-ws_idsa.append(sys.argv[1])
-obj_list = ws_client.list_objects({'ids' : ws_idsa}) 
-for obj in obj_list:
-	print obj
