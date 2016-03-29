cf8128497a8fddaa8b8c3068d0cbd6b9df738d91
kingb12 <kingb12@cs.washington.edu>
2016-03-29 22:48:53 +0000

    added venn2, ran some analyses, factored code in reaction_analysis out

--- lib/Analysis.py
+++ lib/Analysis.py
@@ -8,17 +8,30 @@ def reaction_analysis(model_identities):
     for m in model_identities:
         model = Helpers.get_object(m[0], m[1])
         models.append((m[2], frozenset([Helpers.get_rxn_id(r) for r in Model.get_reactions(model['data'])])))
-    all_models = set(models)
+    return venn_analysis(models)
+
+def venn_analysis(list_of_set_pairs):
+    """
+    Performs a venn analysis on a list of sets (as tuples with labels at tup[0])
+
+    Imagine this as a venn diagram, but any dimensions are possible. Returns a dictionary with the elements belonging
+    to each section of the supposed diagram and a label identifying which sets contribute to this area.
+
+    :param list_of_set_pairs: A list of tuples, where each tuple has a set object at tup[1] and a name for the set at tup[0]
+    :return: a dictionary with a frozen set of names as a key to the elements from the sets in these keys and not others
+    """
+    all_sets = set(list_of_set_pairs)
     rxn_sets = dict()
-    for model_set in powerset(models):
-        if len(model_set) > 0:
-            rxn_set = model_set[0][1]
-            keys = set(model_set)
-            skips = all_models - keys
+    # for each subset of the power set of the sets passed as arguments...
+    for powsub_set in powerset(list_of_set_pairs):
+        if len(powsub_set) > 0:
+            rxn_set = powsub_set[0][1]
+            keys = set(powsub_set)
+            skips = all_sets - keys
             key = set()
-            for m in model_set:
+            for m in powsub_set:
                 key.add(m[0])
-            for m in model_set:
+            for m in powsub_set:
                 rxn_set = rxn_set.intersection(m[1])
             for m in skips:
                 rxn_set = rxn_set - m[1]
@@ -44,13 +57,17 @@ def gene_percents(model, rxn_analysis):
             percentage = 0.0
         result[group] = percentage
     return result
+
+
 def powerset(lst):
     result = [[]]
     for x in lst:
         result.extend([subset + [x] for subset in result])
     return result
+
+
 def three_model_venn(model_identities, filename, genes=False, morphed_model=None, title=None):
-    rxn_sets=[]
+    rxn_sets = []
     for m in model_identities:
         rxn_sets.append(set([Reaction.get_rxn_id(r) for r in Model.get_reactions(Helpers.get_object(m[0], m[1])['data'])]))
     gene_dict = None
@@ -62,7 +79,7 @@ def three_model_venn(model_identities, filename, genes=False, morphed_model=None
             key = 0
             for i in range(0, 3):
                 if model_identities[i][2] in model_set:
-                   key += int(math.pow(10, i))
+                    key += int(math.pow(10, i))
             key = str(key)
             while len(key) < 3:
                 key = '0' + key
--- lib/Plotter.py
+++ lib/Plotter.py
@@ -5,6 +5,9 @@ import matplotlib_venn as venn
 from matplotlib import pyplot as plt
 
 venn3_keys = ['100', '010' ,'001', '110', '011', '101', '111']
+venn2_keys = ['10', '01', '11']
+
+
 def venn3(subsets, title, unit_title, filename, set_labels=None, normalize=1.0, annotation=None):
     plt.figure()
     v = venn.venn3_unweighted(subsets=subsets, set_labels=set_labels)
@@ -19,10 +22,34 @@ def venn3(subsets, title, unit_title, filename, set_labels=None, normalize=1.0, 
             subtitle += 's'
         text.set_text(text.get_text() + '\n' + subtitle)
         text.set_size(text.get_size() - 2)
-    if(annotation is not None):
+    if annotation is not None:
         for a in annotation:
             text = v.get_label_by_id(a)
             xy= text.get_position() - np.array([0, 0.085])
+            plt.annotate(annotation[a], xy=xy, xytext=xy, ha='center', textcoords='offset points', color='r', weight='bold')
+
+    plt.title(title)
+    plt.savefig(filename)
+    plt.close()
+
+
+def venn2(subsets, title, unit_title, filename, set_labels=None, normalize=1.0, annotation=None):
+    plt.figure()
+    v = venn.venn2(subsets=subsets, set_labels=set_labels)
+    for i in range(len(venn2_keys)):
+        label_id = venn2_keys[i]
+        text = v.get_label_by_id(label_id)
+        text.set_position(text.get_position() + np.array([0, 0.02]))
+        # TEMPORALLY COUPLED WITH CREATION OF DIAGRAM
+        subtitle = unit_title
+        if text.get_text() != '1':
+            subtitle += 's'
+        text.set_text(text.get_text() + '\n' + subtitle)
+        text.set_size(text.get_size() - 2)
+    if annotation is not None:
+        for a in annotation:
+            text = v.get_label_by_id(a)
+            xy = text.get_position() - np.array([0, 0.085])
             plt.annotate(annotation[a], xy=xy, xytext=xy, ha='center', textcoords='offset points', color='r', weight='bold')
 
     plt.title(title)
