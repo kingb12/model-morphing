00a088257839e77627f4d90390687b8c5674b9e6
Brendan King <kingb12@cs.washington.edu>
2016-12-18 15:16:17 -0800

    edits for local FBA

--- lib/LocalFbaModelServices.py
+++ lib/LocalFbaModelServices.py
@@ -8,33 +8,33 @@ def call(function, parameters):
 
 
 def gapfill_model(args):
-    call("gapfill_metabolic_model", args)
+    return call("gapfill_metabolic_model", args)
 
 
 def translate_model(args):
-    call("propagate_model_to_new_genome", args)
+    return call("propagate_model_to_new_genome", args)
 
 
 def add_reactions(args):
-    call("edit_metabolic_model", args)
+    return call("edit_metabolic_model", args)
 
 
 def edit_reactions(args):
-    call("edit_metabolic_model", args)
+    return call("edit_metabolic_model", args)
 
 
 def remove_reactions(args):
-    call("edit_metabolic_model", args)
+    return call("edit_metabolic_model", args)
 
 
 def compare_models(args):
-    call("compare_models", args)
+    return call("compare_models", args)
 
 
 def build_metabolic_model(args):
-    call("build_metabolic_model", args)
+    return call("build_metabolic_model", args)
 
 
 def runfba(args):
-    call("run_flux_balance_analysis", args)
+    return call("run_flux_balance_analysis", args)
 
