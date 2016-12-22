0f3d4f3bc6ff10469d36265dffd2d68a9f17ed2c
Brendan King <kingb12@cs.washington.edu>
2016-12-21 21:29:50 -0800

    info digestion

--- lib/LocalFbaModelServices.py
+++ lib/LocalFbaModelServices.py
@@ -8,33 +8,33 @@ def call(function, parameters):
 
 
 def gapfill_model(args):
-    return zip(call("gapfill_metabolic_model", args))
+    return dict(call("gapfill_metabolic_model", args))
 
 
 def translate_model(args):
-    return zip(call("propagate_model_to_new_genome", args))
+    return dict(call("propagate_model_to_new_genome", args))
 
 
 def add_reactions(args):
-    return zip(call("edit_metabolic_model", args))
+    return dict(call("edit_metabolic_model", args))
 
 
 def edit_reactions(args):
-    return zip(call("edit_metabolic_model", args))
+    return dict(call("edit_metabolic_model", args))
 
 
 def remove_reactions(args):
-    return zip(call("edit_metabolic_model", args))
+    return dict(call("edit_metabolic_model", args))
 
 
 def compare_models(args):
-    return zip(call("compare_models", args))
+    return dict(call("compare_models", args))
 
 
 def build_metabolic_model(args):
-    return zip(call("build_metabolic_model", args))
+    return dict(call("build_metabolic_model", args))
 
 
 def runfba(args):
-    return zip(call("run_flux_balance_analysis", args))
+    return dict(call("run_flux_balance_analysis", args))
 
