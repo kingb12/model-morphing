e7f8d337adcb51f335740c5d403777527f97448a
Brendan King <kingb12@cs.washington.edu>
2016-12-21 21:45:40 -0800

    info digestion

--- lib/LocalFbaModelServices.py
+++ lib/LocalFbaModelServices.py
@@ -8,33 +8,41 @@ def call(function, parameters):
 
 
 def gapfill_model(args):
-    return dict(call("gapfill_metabolic_model", args))
+    info = call("gapfill_metabolic_model", args)
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def translate_model(args):
-    return dict(call("propagate_model_to_new_genome", args))
+    info = dict(call("propagate_model_to_new_genome", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def add_reactions(args):
-    return dict(call("edit_metabolic_model", args))
+    info = dict(call("edit_metabolic_model", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def edit_reactions(args):
-    return dict(call("edit_metabolic_model", args))
+    info = dict(call("edit_metabolic_model", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def remove_reactions(args):
-    return dict(call("edit_metabolic_model", args))
+    info = dict(call("edit_metabolic_model", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def compare_models(args):
-    return dict(call("compare_models", args))
+    info = dict(call("compare_models", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def build_metabolic_model(args):
-    return dict(call("build_metabolic_model", args))
+    info = dict(call("build_metabolic_model", args))
+    return dict(zip(info[0::2], info[1::2]))
 
 
 def runfba(args):
-    return dict(call("run_flux_balance_analysis", args))
+    info = dict(call("run_flux_balance_analysis", args))
+    return dict(zip(info[0::2], info[1::2]))
 
